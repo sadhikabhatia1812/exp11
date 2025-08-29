46eb8db3ff7efdaf984e9789205b1dfef42312ce
sadhikabhatia1812 <artermis1806@gmail.com>
2025-08-29 00:03:13 -0400

    Create 11.1.cpp

---
+++ 11.1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class cube{
+    public:
+    int height = 2;
+    int width = 3;
+    int length = 5;
+};
+int main (){
+    cube c1;
+    int vol=c1.height*c1.width*c1.length;
+    cout<< "volume  " << vol << endl;
+}
+
+/* OUTPUT:
+volume  30  
+*/
